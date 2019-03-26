03b6ae84e1792fa1d5855c6e209d10cd15441fb6
Tyrone Shine <tyronewshine@gmail.com>
2019-03-25 22:14:58 -0400

    Redid the layout with CSS grid. Used a bunch of CSS variables. Removed the CRT animations. Added a README for some explanation since the link originally just reloaded the site. Minor JS clean up.

---
+++ README.md
@@ -0,0 +1,55 @@
+# tyronekinda.works
+
+## Intro
+
+Here for public view. Can look through the old stuff for a look into my thoughts through coding. Maybe?
+
+If you didn't get here from my portfolio, I wonder why you are here.
+
+## Why 
+
+Whether I am doing pixel art, websites, rearranging my room, there is always an urge to redo things. My own self assessment of my constant growth. I do this with some of my pixel art, so why not do it with my portfolio? I've already lost enough to the ether. Don't really need to worry about that since it's on GitHub.
+
+## Changelog
+
+Current. What was the last one? It's been going on for long enough that I've lost some. Oh well.
+
+###### v0.06558.72
+
+ - Redone with the vast majority of the positioning done via grid and flexbox.
+ - Removed CRT animations because they can't be lower powered machines. Special thanks to my second hand, Mac Book Pro circa 2007 with Ubuntu installed. You tried really hard.
+   
+
+   **Luckily is goes faster and I now know how to do CSS3-esque keyframes in `anime.js`. So that's a plus.**
+ - Also changed the typeface to Inter. I'm really liking it and changed my desktop and phone to the font as well. *It's really good.*
+ - Cleaned up Javascript: including the utility select function that took *way* longer to get right than expected. \*shakes fist at call & overthinking\*
+
+###### v0.04863.72
+
+- Logo was designed to bring one of my most distinct feature: my under eye bags!
+- Use pixel art. 
+- Slightly jarring colors. 
+- Small tinkering with grid.
+- Mostly absolute positioning goofs.
+- More of a creative dump to see if I can mash together my favorite colors and pixel art into a website.
+- Was originally going to use WebGL for animation. Didn't work out.
+
+###### v0.04251.72
+
+- Went with an even more minimalist approach. White background with a desaturated burgundy.
+- Logo was based off of Persona 5's UX. If you don't know about it, look it up. It's of prodigious quality. 11 on 10. Would recommend.
+
+###### v0.03373.72
+
+- Lighter colors to go with some good professional milestones.
+- Kept scribble logo since I felt I was still lost in the sea that is front-end development.
+
+###### v0.03370.72
+
+ - Dark.
+ - Generic laptop background.
+ - Scribbled logo was meant to convey lost and confusion.
+
+###### Prior to v.03370.72
+
+- Lost to the ether.
--- js/global.js
+++ js/global.js
@@ -1,25 +1,28 @@
 const init = () => {
     'use strict'
     // Variables
-    let container = select('.container')[0]
-    let content = select('.content', container)[0]
+    let container = select('.container')
+    let content = select('.content', container)
     let navItem = select('.nav__item')
-    let breathContainer = select('.bg-container__breath')[0]
-    let wakeContainer = select('.bg-container__wake')[0]
-    let oneFrame = 133; // Actually 8 frames at 60FPS, in milliseconds
-    anime.easings['frameAnimation'] = () => 0
+    let breathContainer = select('.bg-container__breath')
+    let wakeContainer = select('.bg-container__wake')
+    // Animation needs to be a function based value
+    // Function inside a function
+    let oneFrame = 133
+    let frameAnimation = () => (t) => 0;
 
     // Store the initial content so we can revisit it later
     history.replaceState(null, null, document.location.href);
 
     navItem.forEach( el => {
-        let navLink = select('a', el)[0]
+        let navLink = select('a', el)
+        let navBar = select('.nav__link-bar', navLink)
 
         if ( !isTouchDevice() ){
             // Hover animation
-            el.addEventListener('mouseenter', e => navHover('0px', '100%') )
+            navLink.addEventListener('mouseenter', e => navHover(navBar, '110%') )
             
-            el.addEventListener('mouseleave', e => navHover() )
+            navLink.addEventListener('mouseleave', e => navHover(navBar) )
         }
 
         // History
@@ -39,35 +42,45 @@ const init = () => {
         
     })
 
+    window.addEventListener('popstate', e => {
+        // URL check
+        let urlReg = /[^\/]+(?=\/|$)/ig
+        let url = window.location.href
+        let subDirectory = url.match(urlReg)[2]
+
+        return (subDirectory === undefined) ? updateContainers('home', '/') : updateContainers(subDirectory, url)
+    })
+
+    // Breating Animation
     const breathAnimation = anime({
-            targets: '.bg-container__breath svg',
-            viewBox: [ 
-                { value: '0 0 52 35', duration: (oneFrame * 5) }, 
-                { value: '0 0 52 35', duration: oneFrame },
-                { value: '52 0 52 35', duration: oneFrame }, 
-                { value: '104 0 52 35', duration: oneFrame }, 
-                { value: '156 0 52 35', duration: oneFrame }, 
-                { value: '156 0 52 35', duration: (oneFrame * 5) }, 
-            ],
-            duration: oneFrame * 14,
-            easing: 'frameAnimation',
-            direction: 'alternate',
-            loop: 6,
-            complete: () => {
-
-                if ( container.classList.contains('is-ready') ) {
-                    breathAnimation.restart()
-                } else {
-                    breathContainer.classList.add('is-hidden')
-                    wakeContainer.classList.remove('is-hidden')
-                    wakeupAnimation.play()
-                }
+        targets: select('svg', breathContainer),
+        viewBox: [ 
+            { value: '0 0 52 37', duration: (oneFrame * 5) }, 
+            { value: '0 0 52 37', duration: oneFrame },
+            { value: '52 0 52 37', duration: oneFrame }, 
+            { value: '104 0 52 37', duration: oneFrame }, 
+            { value: '156 0 52 37', duration: oneFrame }, 
+            { value: '156 0 52 37', duration: (oneFrame * 5) }, 
+        ],
+        duration: oneFrame * 14,
+        easing: frameAnimation,
+        direction: 'alternate',
+        loop: 2,
+        autoplay: true,
+        complete: () => {
+            if ( container.classList.contains('is-ready') ) {
+                breathAnimation.restart({loop: true})
+            } else {
+                breathContainer.classList.add('is-hidden')
+                wakeContainer.classList.remove('is-hidden')
+                wakeupAnimation.play()
             }
-                
-        })
+        }      
+    })
 
+    // Wakeup Animation
     const wakeupAnimation = anime({
-         targets: '.bg-container__wake svg',
+         targets: select('svg', wakeContainer),
          viewBox: [ 
              { value: '0 0 52 37', duration: (oneFrame * 5) }, 
              { value: '0 0 52 37', duration: oneFrame },
@@ -80,102 +93,104 @@ const init = () => {
              { value: '312 0 52 37', duration: (oneFrame * 5) }, 
          ],
          duration: oneFrame * 17,
-         easing: 'frameAnimation',
+         easing: frameAnimation,
          direction: 'alternate',
          autoplay: false,
          complete: () => {
-
             breathContainer.classList.remove('is-hidden')
             wakeContainer.classList.add('is-hidden')
             container.classList.add('is-ready')
             breathAnimation.restart()
-
+            textAnimation.play()
          }
      })
-
-    window.addEventListener('popstate', (e) => {
-        // console.log(e.state)
-        // URL check
-        // let urlReg = /[^\/]+(?=\/$|$)/ig
-        let urlReg = /[^\/]+(?=\/|$)/ig
-        let url = window.location.href
-        let subDirectory = url.match(urlReg)[2]
-
-        if ( subDirectory === undefined) {
-            updateContainers('home', '/')
-        } else {
-            updateContainers(subDirectory, url)
-        }
-    })
 }
 
-const select = (selector, parent = document) => (
-    [].slice.call(
-        parent.querySelectorAll(selector)
-    )
-)
-
-const navHover = (bottom = '4px', height = '10%') => {
-     anime.remove('.is-active .nav__link-bar')
-     anime({
-         targets: '.is-active .nav__link-bar',
-         bottom: bottom,
-         height: height,
-         duration: 660,
-         elasticity: 300
-     })
+const select = (selector, parent = document) => {
+    // Don't need slice.call or map if using spread. It appends junk itself.
+    // However it does not return an active node list. Tragic.
+    let foundEl = [...parent.querySelectorAll(selector)]
+    return (foundEl.length === 1) ? foundEl[0] : foundEl
 }
+
+// Text Animation
+// Steps so it feels like an old game's junk fading in
+const textAnimation = anime({
+    targets: select('.bg-container__text span'),
+    opacity: [0, 1],
+    easing: 'steps(3)',
+    delay: anime.stagger(600),
+    autoplay: false
+})
 
 // Device check for custom cursor, if uses touch, ignore
 const isTouchDevice = () => {
      return 'ontouchstart' in window || navigator.maxTouchPoints // works on IE10/11 and Surface
  }
 
- const getContent = (element, url) => {
-    var request = new XMLHttpRequest();
+const navHover = (selector, height = '10%') => {
+    anime.remove(selector)
+    anime({
+        targets: selector,
+        bottom: '0',
+        height: height,
+        duration: 460,
+        elasticity: 300
+    })
+}
 
-    request.onreadystatechange = () => {
-        if (request.readyState === XMLHttpRequest.DONE) {
-           if (request.status === 200) {
-               element.innerHTML = request.responseText.match(/.*<section class="content".*>([\s\S]*)<\/section>.*/)[0].replace(/.*<section class="content".*>*/, '').replace(/<\/section>.*/, '')
-           }
-        }
-    };
+// Now returns a promise!
+const getContent = (element, url) => {
+    let prom = new Promise( (resolve, reject) => {
 
-    request.open('GET', url, true);
-    request.send();
+        // Chuck the request in here and handle to stuff on success
+        // Like promises do
+        let request = new XMLHttpRequest()
+        request.open('GET', url)
+        request.onload = () => resolve(request.responseText)
+        request.onerror = () => reject(request.statusText)
+        request.send()
+
+    }).then( (val) => {
+
+        let newContent = val.match(/.*<article class="content grid-layout".*>([\s\S]*)<\/article>.*/)[0].replace(/.*<article class="content grid-layout".*>*/, '').replace(/<\/article>.*/, '')
+        element.innerHTML = newContent;
+
+    }).catch( (reason) => {
+        element.innerHTML = reason;
+    })
 }
 
 const updateContainers = (page, url) => {
     // Variables
-    let body = select('body')[0]
-    let container = select('.container')[0]
-    let content = select('.content')[0]
+    let body = select('body')
+    let container = select('.container')
+    let content = select('.content')
     let nav = select('.nav__item')
-    // Begin screen off CRT transition
-    body.classList.replace('screen-on', 'screen-off')
 
     // Change content on a delay, just to get rid of the weird occasional pop in
+    // Update container class
     setTimeout( () => {
-        // Update container class
+        content.innerHTML = '';
         container.classList = ''
-        container.classList.add('container', 'container-' + page, 'is-ready')
+        container.classList.add('container', 'container-' + page, 'is-ready', 'grid-layout')
 
         // Keep the nav in the DOM because it borks and reloads. 
         // There's also a way to have it work with it, but simple class change works.
         nav.forEach( el =>{
             if (el.classList.contains('nav__item--' + page)) {
                 el.classList.replace('is-active', 'is-hidden')
+                el.setAttribute('aria-hidden', 'true')
             } else {
                 el.classList.replace('is-hidden', 'is-active')
+                el.setAttribute('aria-hidden', 'false')
             }
         })
-
+        
+        textAnimation.seek(textAnimation.duration);
         getContent(content, url)
-
-        // Turn on that screen!
-        body.classList.replace('screen-off', 'screen-on')
-    }, 800);
+    }, 200);  
 }
+ 
 
 document.addEventListener('DOMContentLoaded', init)
